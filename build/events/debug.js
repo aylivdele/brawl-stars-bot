13db4464065eb802c0a8e43739258415f3f8ea92
Poon <ivanholin479@gmail.com>
2023-06-25 20:02:41 +0500

    build

--- build/events/debug.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const event = {
-    name: "debug",
-    execute: (debug) => {
-        console.log(debug);
-    }
-};
-exports.default = event;
