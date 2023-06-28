79ef7303e1de735389a96c15a79755355b5f898d
aylivdele <irassomahin@gmail.com>
2023-06-28 21:40:24 +0700

    add express

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {Command, SlashCommand} from "./types";
 import {config} from "dotenv";
 import {readdirSync} from "fs";
 import {join} from "path";
+import express, { Request, Response } from "express";
 
 const { Guilds, MessageContent, GuildMessages, GuildMembers, DirectMessages } = GatewayIntentBits
 const client = new Client({intents:[Guilds, MessageContent, GuildMessages, GuildMembers, DirectMessages], partials: [Partials.Channel]});
@@ -31,4 +32,15 @@ client.login(process.env.TOKEN)
     console.error('Login exception');
     console.error(error);
 })
+
+const app = express();
+const port = process.env.PORT;
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running on port: ${port}`);
+});
+
 
